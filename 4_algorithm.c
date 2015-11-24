2fb2678ec58064493e09b661b925d031f8d2453e
zhangly <dear531@21cn.com>
2015-11-24 18:29:58 +0800

    mid expression to suffix expression

--- 4_algorithm.c
+++ 4_algorithm.c
@@ -104,7 +104,7 @@ static inline int is_expre(char c)
 	return '+' == c || '-' == c || '*' == c || '/' == c;
 }
 
-static inline int is_flet(char c)
+static inline int is_left(char c)
 {
 	return '(' == c;
 }
@@ -116,6 +116,8 @@ static inline int is_right(char c)
 
 static int check_expre(struct expre *expre)
 {
+	if (NULL == expre)
+		return -1;
 	int i;
 	char c1, c2;
 	/* check head */
@@ -148,6 +150,8 @@ failure:
 
 int check_bracets(struct expre *expre)
 {
+	if (NULL == expre)
+		return -1;
 	struct stack *top = NULL;
 	int i;
 	init_stack(&top);
@@ -172,6 +176,20 @@ failure:
 	return 1;
 }
 
+#define CHECK_EXPRE(c) ('+' != c && '-' != c  && '*' != c  && '/' != c )
+int cmp_expre(char c1, char c2)
+{
+	if (CHECK_EXPRE(c1) || CHECK_EXPRE(c2))
+		return -2;
+	if (('+' == c1 || '-' == c1) && ('*' == c2 || '/' == c2))
+		return -1;
+	else if (('*' == c1 || '/' == c1) && ('+' == c2 || '-' == c2))
+		return 1;
+	else
+		return 0;
+}
+#undef CHECK_EXPRE
+
 int main(void)
 {
 #if 1
@@ -194,11 +212,13 @@ int main(void)
 			p = pp;
 		} else if (is_expre(*p)) {
 			EXPRE_OPRAT(EXPRE);
-		} else if (is_flet(*p)) {
+		} else if (is_left(*p)) {
 			EXPRE_OPRAT(LEFT);
 		} else if (is_right(*p)) {
 			EXPRE_OPRAT(RIGHT);
 		} else {
+			fprintf(stderr, "invalid expression char %c\n",
+					*p);
 			return -1;
 		}
 	}
@@ -211,6 +231,76 @@ int main(void)
 		fprintf(stderr, "check bracets is failure\n");
 		return 1;
 	}
+	struct expre suffix[64] = {0};
+	struct stack *top = NULL;
+	int j;
+	char tmpc;
+	init_stack(&top);
+	for (i = 0, j = 0; 0 != expre[i].c && 0 != expre[i].exp_num; i++) {
+		print_stack(top);
+		if (NUMBER == expre[i].exp_num) {
+			suffix[j++] = expre[i];
+		} else if (LEFT == expre[i].exp_num) {
+			push_stack(&top, expre[i].c);
+		} else if (!is_empty(top) && RIGHT == expre[i].exp_num) {
+				pop_stack(&top, &tmpc);
+			while ('(' != tmpc) {
+				suffix[j].c = tmpc;
+				suffix[j].exp_num = EXPRE;
+				j++;
+				if (is_empty(top))
+					break;
+				pop_stack(&top, &tmpc);
+			}
+		} else if (!is_empty(top) && EXPRE == expre[i].exp_num) {
+			int ret;
+			pop_stack(&top, &tmpc);
+			if ('(' == tmpc) {
+				push_stack(&top, tmpc);
+				push_stack(&top, expre[i].c);
+			} else if (-2 == (ret = cmp_expre(expre[i].c, tmpc))) {
+				fprintf(stderr, "%c or %c is invalid char\n",
+						tmpc, expre[i].c);
+				return -1;
+			} else if (-1 == ret) {
+				suffix[j].c = tmpc;
+				suffix[j].exp_num = EXPRE;
+				j++;
+				while (!is_empty(top)) {
+					pop_stack(&top, &tmpc);
+					if ('(' == tmpc) {
+						push_stack(&top, tmpc);
+						break;
+					}
+					suffix[j].c = tmpc;
+					suffix[j].exp_num = EXPRE;
+					j++;
+				}
+				push_stack(&top, expre[i].c);
+			} else {
+				push_stack(&top, tmpc);
+				push_stack(&top, expre[i].c);
+			}
+		} else if (is_empty(top) && EXPRE == expre[i].exp_num) {
+				push_stack(&top, expre[i].c);
+		}
+	}
+	while (!is_empty(top)) {
+		pop_stack(&top, &tmpc);
+		suffix[j].c = tmpc;
+		suffix[j].exp_num = EXPRE;
+		j++;
+	}
+	for (i = 0; 0 != suffix[i].c && 0 != suffix[i].exp_num; i++) {
+		if (NUMBER == suffix[i].exp_num) {
+			fprintf(stdout, "suffix[%d].c:%d\n",
+					i, suffix[i].c);
+		} else {
+			fprintf(stdout, "suffix[%d].c:%c\n",
+					i, suffix[i].c);
+		}
+	}
+	destroy_stack(&top);
 #endif
 	return 0;
 }
