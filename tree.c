3d006e3915666e1d2faaebfad96c5b990e815602
zhangly <dear531@21cn.com>
2015-11-30 21:10:25 +0800

    level order for two child tree use two queue

--- tree.c
+++ tree.c
@@ -76,76 +76,91 @@ static int count(struct tree *tree)
 			count(tree->right);
 }
 
-struct stack {
-	int top1;
-	int top2;
+struct queue {
 	struct tree** node;
+	int front;
+	int rear;
+	int flag;
 	int size;
 };
 
-int init_stack(struct stack *stack, int data_size)
+int init_queue(struct queue *queue, int size)
 {
-	if (NULL == (stack->node= 
-		malloc(sizeof(*stack->node) * (data_size + 1)))) {
-		fprintf(stderr, "init_stack error :%s\n",
+	if (NULL == (queue->node = malloc(sizeof(queue->node) * size))) {
+		fprintf(stderr, "malloc error :%s\n",
 				strerror(errno));
-		return -1;
+		exit(EXIT_FAILURE);
 	}
-	stack->top1 = -1;
-	stack->top2 = data_size;
-	stack->size = data_size;
+	queue->size = size;
+	queue->front = 0;
+	queue->rear = 0;
+	queue->flag = 0;
 	return 0;
 }
 
-int destroy_stack(struct stack *stack)
+int destroy_queue(struct queue *queue)
 {
-	if (NULL != stack->node) {
-		free(stack->node);
-		stack->node = NULL;
+	if (NULL != queue->node) {
+		free(queue->node);
+		queue->node = NULL;
 	}
-	stack->size = 0;
+	queue->size = 0;
+	queue->front = 0;
+	queue->rear = 0;
+	queue->flag = 0;
 	return 0;
 }
 
-static int is_stack1_empty(struct stack *stack)
+int is_queue_empty(struct queue *queue)
 {
-	return stack->top1 == -1;
+	return queue->front == queue->rear && 0 == queue->flag;
 }
 
-static int is_stack2_empty(struct stack *stack)
+int is_queue_full(struct queue *queue)
 {
-	return stack->top2 == stack->size;
+	return queue->front == queue->rear && 1 == queue->flag;
 }
 
-static void stack1_push(struct stack *stack, struct tree *node)
+void enqueue(struct queue *queue, struct tree *node)
 {
-	stack->top1++;
-	stack->node[stack->top1] = node;
+	queue->node[queue->rear] = node;
+	queue->rear = (queue->rear + 1) % queue->size;
+	queue->flag = 1;
 }
 
-static void stack2_push(struct stack *stack, struct tree *node)
+void dequeue(struct queue *queue, struct tree **node)
 {
-	stack->top2--;
-	stack->node[stack->top2] = node;
+	if (NULL != node)
+		*node = queue->node[queue->front];
+	queue->front = (queue->front + 1) % queue->size;
+	queue->flag = 0;
 }
 
-static struct tree *stack1_pop(struct stack *stack)
+int queue_length(struct queue *queue)
 {
-	return stack->node[stack->top1--];
+	return (queue->size + queue->rear - queue->front);
 }
 
-static struct tree *stack2_pop(struct stack *stack)
+void print_queue(struct queue *queue)
 {
-	return stack->node[stack->top2++];
-}
-
-static int is_stack_full(struct stack *stack)
-{
-	return stack->top1 + 1 == stack->top2;
+	int i, start;
+	if (is_queue_full) {
+		fprintf(stdout, "queue->node[%d]:%p\n",
+			queue->front, queue->node[queue->front]);
+		start = queue->front + 1;
+	} else {
+		start = queue->front;
+	}
+	for (i = start; i != queue->rear;
+		i = (i + 1) % queue->size) {
+		fprintf(stdout, "queue->node[%d]:%p\n",
+			i, queue->node[i]);
+	}
 }
 
 int level_tree(struct tree *tree, int size)
 {
+#if 0
 	int max_number = (log2(size) + 1) * 2;
 	struct stack stack;
 	init_stack(&stack, max_number);
@@ -171,7 +186,52 @@ int level_tree(struct tree *tree, int size)
 		stack_empty = (stack_empty == is_stack1_empty ? is_stack2_empty : is_stack1_empty);
 	}
 	destroy_stack(&stack);
-	return ;
+#endif
+#if 0
+	struct tree *t[10];
+	int i;
+	struct queue queue1;
+	init_queue(&queue1, sizeof(t) / sizeof(&t));
+	fprintf(stdout, "is empty :%d\n", is_queue_empty(&queue1));
+	for (i = 0; i < sizeof(t) / sizeof(*t); i++) {
+		if (!is_queue_full(&queue1))
+			enqueue(&queue1, t[i]);
+	}
+	print_queue(&queue1);
+	dequeue(&queue1, NULL);
+	enqueue(&queue1, (struct tree *)(1));
+	print_queue(&queue1);
+	fprintf(stdout, "queue length :%d\n",
+			queue_length(&queue1));
+	fprintf(stdout, "is full :%d\n", is_queue_full(&queue1));
+#endif
+#if 1
+	struct queue queue1, queue2;
+	int max_number = (log2(size) + 1) * 2;
+	init_queue(&queue1, max_number);
+	init_queue(&queue2, max_number);
+	enqueue(&queue1, tree);
+	struct tree *t;
+	struct queue *qp1 = &queue1, *qp2 = &queue2, *qtmp;
+	int count = 0;
+	while (!is_queue_empty(&queue1) || !is_queue_empty(&queue2)) {
+		while (!is_queue_empty(qp1)) {
+			dequeue(qp1, &t);
+			if (t != NULL)
+				fprintf(stdout, "%d ", t->data);
+			if (!is_queue_full(qp2) && NULL != t->left)
+				enqueue(qp2, t->left);
+			if (!is_queue_full(qp2) && NULL != t->right)
+				enqueue(qp2, t->right);
+		}
+		fprintf(stdout, "\n");
+		qtmp = qp1; qp1 = qp2; qp2 = qtmp;
+	}
+	destroy_queue(&queue1);
+	destroy_queue(&queue2);
+#endif
+	return 0;
+
 }
 
 int main(void)
