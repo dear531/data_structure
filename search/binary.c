04a6a80e00d018955e0904f3174874df1f10cc3b
zhangly <dear531@21cn.com>
2015-12-09 18:55:22 +0800

    optimized mid value search for balance destribution

--- search/binary.c
+++ search/binary.c
@@ -2,14 +2,15 @@
 
 int main(void)
 {
-	int a[1];
-	int i, tmp, search = 0;
+	int a[1000];
+	int i, tmp, search = 8;
 	for (i = 0; i < sizeof(a) / sizeof(*a); i++) {
 		a[i] = 2 * i;
 	}
 	int begin = 0, mid, end = sizeof(a) / sizeof(*a) - 1;
 	while (begin <= end) {
-		mid = (begin + end) / 2;
+		/* optimized mid value search for balance destribution */
+		mid = (end - begin) * (search - a[begin]) / (a[end] - a[begin]);
 		if (search > a[mid]) {
 			begin = mid + 1;
 		} else if (search < a[mid]) {
